120de8132399b860a03d360ff2f19b09237720c4
Bruno de Andrade Dinis <bruno.a.dinis96@gmail.com>
2023-09-18 16:18:27 -0300

    Evento carrossel nos artigos

---
+++ assets/js/artigos.js
@@ -0,0 +1,22 @@
+let lista = document.querySelectorAll("div .blog-post__button a[href]");
+let itemzin = document.querySelector("div .blog-post__button a[href]").href.split("#");
+
+console.log(itemzin)
+let div = document.getElementById(itemzin[1])
+console.log(div.parentElement.parentElement)
+
+console.log(lista);
+
+lista.forEach(a => {
+    a.addEventListener("click", event => {
+        let item = event.target.href.split("#");
+        console.log(item);
+        let div = document.getElementById(item[1])
+        console.log(div)
+
+        item = event.target
+        console.log(item.parentElement.parentElement.style.display = "none")
+
+        div.style.display = "flex"
+    });
+});
